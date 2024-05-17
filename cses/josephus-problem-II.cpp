90ead26789190f91258cc2ee1c5a61885a8160f5
Zafir <zafir.nasim@gmail.com>
2024-05-16 22:11:21 -0500

    add cses josephus problem II

--- cses/josephus-problem-II.cpp
+++ cses/josephus-problem-II.cpp
@@ -1,3 +1,6 @@
+#include <bits/stdc++.h>
+using namespace std;
+
 struct Segtree {
     int N=1;
     using T = int;
@@ -27,14 +30,29 @@ struct Segtree {
     }
     // find i where op(segtree[0:i]) = val
     int search(T val) {
-        int i=1,s=0;
-        while (i<N) {
-            if (s+tree[2*i]<val) {
-                s+=tree[2*i];
-                i=2*i+1;
-            } else
-                i=2*i;
-        }
-        return i-N;
+    	int i=1,s=0;
+    	while (i<N) {
+    		if (s+tree[2*i]<val) {
+    			s+=tree[2*i];
+    			i=2*i+1;
+    		} else
+    			i=2*i;
+    	}
+    	return i-N;
     }
 };
+
+int main() {
+	int n,k;cin>>n>>k;
+	Segtree st(n);
+	for (int i=0;i<n;i++) st.upd(i,1);
+	int target=k%n;
+	for (int i=1;i<=n;i++) {
+		int x = st.search(target+1);
+		st.upd(x,0);
+		cout << x+1 << " ";
+		if (i!=n) target = (target+k)%(n-i);
+	}
+	cout << endl;
+	return 0;
+}
